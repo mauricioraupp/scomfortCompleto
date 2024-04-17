6b49807a529ccfa10c1cda14a4d0348ed8f25edf
Mauricio <mauricio.p.raupp@gmail.com>
2024-04-17 12:26:37 -0300

    aula

--- front/script.js
+++ front/script.js
@@ -1,23 +1,32 @@
 let buttonConnect = document.getElementById("submitNewsletter");
 
-buttonConnect.onclick = async function(e) {
-  e.preventDefault();
+buttonConnect.onclick = async function() {
   let name = document.querySelector("#name").value;
   let email = document.querySelector("#email").value;
-  let data = {name,email};
+  
+  let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  let result = reg.test(email);
 
-  const response = await fetch('http://localhost:3003/api/store/task', {
-    method: "POST",
-    headers: {"Content-type": "application/json;charset=UTF-8"},
-    body: JSON.stringify(data)
+  if (result) {
+    let data = {name,email};
 
-  });
-
-  let content = await response.json();
-
-  if(content.success){
-    alert("Sucesso");
+    const response = await fetch('http://localhost:3003/api/store/task', {
+      method: "POST",
+      headers: {"Content-type": "application/json;charset=UTF-8"},
+      body: JSON.stringify(data)
+  
+    });
+  
+    let content = await response.json();
+  
+    if(content.success){
+      alert("Sucesso");
+    } else {
+      alert("Não");
+    }
   } else {
-    alert("Não");
+    alert("Email inserido é inválido");
   }
+
 }
+
